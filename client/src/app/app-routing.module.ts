ceb38a5a0e8e2b44962f1145dd74c873f126e364
vishwaszambre <zambrevishwas@gmail.com>
2017-07-01 14:55:41 +0530

    Navbar updated

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import {ModuleWithProviders} from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const appRoutes : Routes = [
+const appRoutes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
   },
   {
-    path:'dashboard',
-    component:DashboardComponent
+    path: 'dashboard',
+    component: DashboardComponent
   }
   //{ path: '*', component: PageNotFoundComponent }
 ];
@@ -22,7 +22,6 @@ const appRoutes : Routes = [
   providers: [],
   bootstrap: [],
   exports: [RouterModule]
-})
-*/
+})*/
 export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 //export class AppRoutingModule { }
