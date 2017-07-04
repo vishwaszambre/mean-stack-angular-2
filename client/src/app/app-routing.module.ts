662d190423b03756dba05b6885cf194283b7f631
vishwaszambre <zambrevishwas@gmail.com>
2017-07-04 17:36:01 +0530

    form-validations

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoutes: Routes = [
   {
@@ -12,6 +13,10 @@ const appRoutes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
   }
   //{ path: '*', component: PageNotFoundComponent }
 ];
